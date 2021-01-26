28ac857d5543b288d417d3d6b18696a093870e12
Erwin Luz Leon <eluz@bancoazteca.com>
2021-01-26 14:51:00 -0600

    Initial Commit

---
+++ Delegados/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegados
+//
+//  Created by Erwin on 26/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
