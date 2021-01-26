1af10d99d8d50ca390bece963a2d2424052b6f3d
Erwin Luz Leon <eluz@bancoazteca.com>
2021-01-26 15:20:26 -0600

    se esta atento a al respuesta del segundo view controller

--- Delegados/SegundaViewController.swift
+++ Delegados/SegundaViewController.swift
@@ -1,16 +1,24 @@
 import UIKit
 
+protocol ColorSeleccionDelegate {
+    func didTapChoiseColor(nombre: String)
+}
+
 class SegundaViewController: UIViewController {
+    
+    var seleccionDelegate: ColorSeleccionDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func rojoAction(_ sender: Any) {
+        seleccionDelegate?.didTapChoiseColor(nombre: "rojo")
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func azulAction(_ sender: Any) {
+        seleccionDelegate?.didTapChoiseColor(nombre: "azul")
         self.dismiss(animated: true, completion: nil)
     }
     
--- Delegados/ViewController.swift
+++ Delegados/ViewController.swift
@@ -7,6 +7,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func chooseColorButton(_ sender: Any) {
+        guard let seleccionVC = storyboard?.instantiateViewController(identifier: "SegundaViewController") as? SegundaViewController else {
+            return
+        }
+        seleccionVC.seleccionDelegate = self
+        present(seleccionVC, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: ColorSeleccionDelegate {
+    func didTapChoiseColor(nombre: String) {
+        self.lblOpcion.text = "Escogiste \(nombre)"
+    }
+}
